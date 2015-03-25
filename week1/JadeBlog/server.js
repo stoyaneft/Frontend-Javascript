88d914d49cdd37e692b9efe03e5f878189981c20
stoyaneft <stoyaneft@gmail.com>
2015-03-25 14:32:52 +0200

    Improving structure of JadeBlog

--- week1/JadeBlog/server.js
+++ week1/JadeBlog/server.js
@@ -1,30 +1,53 @@
+"use strict"
+
+// dependancies
 var express = require('express')
+var fs = require("fs");
+var posts = require("./posts.json");
+
+// create app
 var app = express()
 
+
+// configuration and middleware
 app.use(express.static('public'));
-app.set('views', './views')
-app.set('view engine', 'jade')
+app.set('view engine', 'jade');
 
+
+// routes
 app.get('/', function (req, res) {
-  res.render('index', { message: 'Hello there!'});
+  res.render('index', {posts: posts});
 })
-
-var fs = require("fs");
 
 // listen for files: /post -> /views/post.jade
 app.get("/:fileName", function(req, res, next){
-  if(req.params && req.params.fileName && fs.existsSync(__dirname+"/views/"+req.params.fileName+".jade")){
-    res.render(req.params.fileName);
+  if(req.params && req.params.fileName){
+    var fileName = req.params.fileName.replace(".html","");
+
+    // if jade file exists
+    if(fs.existsSync(__dirname+"/views/"+fileName+".jade")){
+      res.render(fileName);
+    // if post is in posts
+    } else if (posts[fileName]) {
+      res.render("post", {post: posts[fileName]});
+    // else continue
+    } else {
+      next();
+    }
+
   } else {
     next();
   }
 })
 
+
+
+// set up server
 var server = app.listen(3000, function () {
 
-var host = server.address().address
-var port = server.address().port
+  var host = server.address().address
+  var port = server.address().port
 
-console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://%s:%s', host, port)
 
 })
