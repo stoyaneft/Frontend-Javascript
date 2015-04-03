ddd14ec2b1868ca20876679a3372db7e59c64298
stoyaneft <stoyaneft@gmail.com>
2015-04-03 19:14:54 +0300

    Updated implementation of queue with closures

--- week3/queue.js
+++ week3/queue.js
@@ -1,4 +1,4 @@
-function Queue(){
+var queue = (function(){
     var container = [];
     return {
         push: function(x){
@@ -8,24 +8,23 @@ function Queue(){
             return container.shift();
         },
         isEmpty: function(){
-            return container == [];
+            return container.length === 0;
         }
     }
-}
+})();
 
-// queue = new Queue();
-// queue.push(2);
-// queue.push(3);
+queue.push(2);
+queue.push(3);
 
-// queue.push(4);
+queue.push(4);
 
-// queue.push(5);
-// queue.push(6);
-// queue.push(7);
-// console.log(queue.pop());
-// console.log(queue.pop());
-// console.log(queue.pop());
-// console.log(queue.pop());
-// console.log(queue.pop());
-// console.log(queue.pop());
-
+queue.push(5);
+queue.push(6);
+queue.push(7);
+console.log(queue.pop());
+console.log(queue.pop());
+console.log(queue.pop());
+console.log(queue.pop());
+console.log(queue.pop());
+console.log(queue.pop());
+console.log(queue.isEmpty());
